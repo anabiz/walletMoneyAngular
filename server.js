1072e64e904ef99b5dc5c58352be70ebc73477a6
anabiz <anabizconcept9@gmail.com>
2021-02-14 20:45:33 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -6,18 +6,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/myNewapp'));
+app.use(express.static(__dirname + '/dist/myNewapp'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/myNewApp/index.html'));
-// });
+res.sendFile(path.join(__dirname+'/dist/myNewApp/index.html'));
+});
 
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'myNewApp', 'index.html');
-    res.sendFile(index);
-  });
-  
+
 
 // Start the app by listening on the default
 app.listen(process.env.PORT || 8080);
