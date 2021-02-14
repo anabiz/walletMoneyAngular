ecde8724929fc9a899322907930dea0a56c1f478
anabiz <anabizconcept9@gmail.com>
2021-02-14 20:21:02 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/pocketmoneyui'));
+app.use(express.static(__dirname + '/dist/myNewapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/pocketmoneyui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/myNewApp/index.html'));
 });
 
 // Start the app by listening on the default
