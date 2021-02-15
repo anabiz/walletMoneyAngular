7d6cefd05f729332a4b483ebc8bab4201caf3e40
anabiz <anabizconcept9@gmail.com>
2021-02-15 11:24:06 +0100

    server js condition for other file types

--- server.js
+++ server.js
@@ -5,12 +5,25 @@ const path = require('path');
 
 const app = express();
 
+const allowed = [
+    '.js',
+    '.css',
+    '.png',
+    '.jpg',
+    '.jpeg'
+  ];
+  
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/myNewapp'));
 
 app.get('*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/myNewApp/index.html'));
+    if (allowed.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+        res.sendFile(path.join(__dirname+`/dist/myNewApp/${req.url}`));
+     } else {
+        res.sendFile(path.join(__dirname+'/dist/myNewApp/index.html'));
+     }
+  
+// res.sendFile(path.join(__dirname+'/dist/myNewApp/index.html'));
 });
 
 
